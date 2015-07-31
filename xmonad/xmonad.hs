720850ae6a4445a7808b2c991486ced37487c56c
tugrulyatagan <tugrulyatagan@gmail.com>
2015-08-01 00:58:29 +0300

    initial xmonad files

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Config.Gnome
+import qualified Data.Map as M
+import XMonad.Hooks.SetWMName
+
+customKeys (XConfig {modMask = modm}) = M.fromList $
+	[ ((modm, xK_p)			, spawn "dmenu_run")
+	, ((modm .|. shiftMask, xK_q)	, spawn "gnome-session-quit") 
+	]
+
+main = do
+    xmonad $ gnomeConfig
+	{ modMask 		= mod4Mask
+	, borderWidth 		= 3
+	, terminal 		= "gnome-terminal"
+	, keys     		= customKeys <+> keys gnomeConfig
+--	, logHook 		= logHook gnomeConfig
+	, manageHook 		= manageDocks <+> manageHook gnomeConfig
+	, startupHook 		= startupHook gnomeConfig >> setWMName "LG3D"
+	}
